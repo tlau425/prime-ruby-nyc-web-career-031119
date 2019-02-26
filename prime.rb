bb441ea72ab9bb0eee5c4a85dd72c6527ebb60a5
Tommy Lau <tlau425@yahoo.com>
2019-02-26 22:02:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(int)
+  prime_num = true
+  if int > 1 
+    for d in 2..(int - 1)
+      if (int % d) == 0
+  prime_num = false
+  return prime_num
+    end
+  end
+else 
+  prime_num = false
+  return prime_num
+  end 
+  return prime_num
+end
