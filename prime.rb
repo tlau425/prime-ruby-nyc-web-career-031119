024962c0956adf1775edf5a6e6eee9673faec6a2
Tommy Lau <tlau425@yahoo.com>
2019-02-26 21:39:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(int)
   prime_num = int % 2
-  if int = 2 || int = -1
+  if int = 2
     return true
-  elsif prime_num = 0
+  elsif prime_num = 0 || prime_num = -1
     return false
   else return true
 end
